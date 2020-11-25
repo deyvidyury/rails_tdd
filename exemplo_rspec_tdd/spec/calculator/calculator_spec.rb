c3e107fd7ad3ac6b546117ca9c7f4af1650e701b
Deyvid Lopes <deyvidyury@yahoo.com.br>
2020-11-25 15:47:14 -0300

    subject

--- exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
+++ exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
@@ -3,20 +3,17 @@ require "calculator"
 describe Calculator do
 	context '#sum' do
 		it 'two positive numbers' do
-			calc = Calculator.new
-			result = calc.sum(5,7)
+			result = subject.sum(5,7)
 			expect(result).to eq(12)
 		end
 
 		it 'two negative numbers' do
-			calc = Calculator.new
-			result = calc.sum(-5,-7)
+			result = subject.sum(-5,-7)
 			expect(result).to eq(-12)
 		end	
 
 		it 'positive and negative numbers' do
-			calc = Calculator.new
-			result = calc.sum(-5,7)
+			result = subject.sum(-5,7)
 			expect(result).to eq(2)
 		end	
 	end
