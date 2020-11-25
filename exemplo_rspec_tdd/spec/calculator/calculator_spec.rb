1242686fde7e4dfabebb78bde19049c609c99fec
Deyvid Lopes <deyvidyury@yahoo.com.br>
2020-11-25 15:42:51 -0300

    refactor test

--- exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
+++ exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
@@ -1,15 +1,24 @@
 require "calculator"
 
 describe Calculator do
-	it 'sum method' do
-		calc = Calculator.new
-		result = calc.sum(5,7)
-		expect(result).to eq(12)
-	end
+	context '#sum' do
+		it 'two positive numbers' do
+			calc = Calculator.new
+			result = calc.sum(5,7)
+			expect(result).to eq(12)
+		end
 
-	it 'sum method with negative numbers' do
-		calc = Calculator.new
-		result = calc.sum(-5,-7)
-		expect(result).to eq(-12)
+		it 'two negative numbers' do
+			calc = Calculator.new
+			result = calc.sum(-5,-7)
+			expect(result).to eq(-12)
+		end	
+
+		it 'positive and negative numbers' do
+			calc = Calculator.new
+			result = calc.sum(-5,7)
+			expect(result).to eq(2)
+		end	
 	end
+	
 end
