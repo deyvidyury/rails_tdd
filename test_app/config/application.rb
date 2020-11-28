5f1942573f2012e23fb468a720359e5532261211
Deyvid Lopes <deyvidyury@yahoo.com.br>
2020-11-28 14:05:24 -0300

    initial setup rspec on rails project

--- test_app/config/application.rb
+++ test_app/config/application.rb
@@ -17,15 +17,23 @@ require "sprockets/railtie"
 Bundler.require(*Rails.groups)
 
 module TestApp
-  class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+	class Application < Rails::Application
+		# Initialize configuration defaults for originally generated Rails version.
+		config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+		# Settings in config/environments/* take precedence over those specified here.
+		# Application configuration should go into files in config/initializers
+		# -- all .rb files in that directory are automatically loaded.
 
-    # Don't generate system test files.
-    config.generators.system_tests = nil
-  end
+		# Don't generate system test files.
+		config.generators.system_tests = nil
+
+		config.generators do |g|
+			g.test_framework :rspec,
+			fixtures: false,
+			view_specs: false,
+			helper_specs: false,
+			routing_specs: false
+		end
+	end
 end
